c0a556971afd17cb8e2f9e1a92136b1381549a8a
Bailosama <natsubarry@gmail.com>
2025-10-03 15:44:47 +0000

    ajout de la route delete post

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,5 @@
 // Routes de l'utilisateur
 const { Router } = require('express');
-const PostController = require('../controllers/userController');
 
 const router = Router();
 
@@ -18,7 +17,6 @@ router.put('/edit/:id', (req, res) => {
     res.send(`Mise à jour de l'utilisateur avec l'ID ${req.params.id}`);
 });
 // route pour supprimer un utilisateur par ID
-router.delete('/:id', PostController.deletePost);
 
 
 module.exports = router;
