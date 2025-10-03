d7550162a2f53a641cb16f4dd35cbcd43b928edb
Mamadou Masroughou Bah <mamadoumasroughoubah@gmail.com>
2025-10-03 16:24:39 +0000

    mise en place de l'endpoint de la lecture

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,32 @@
 const Post = require('../models/Post');
 
 class PostController {
+    // methode pour recuperer tous les posts
+    async getAllPosts(req, res)  {
+        try {
+            const posts = await Post.findAll();
+            res.status(200).json(posts); 
+        }
+        catch (error) {
+            console.error('Erreur lors de la récupération des posts :', error);
+            res.status(500).json({ message: 'Erreur serveur' });
+        };
+    }
+    // methode pour recuperer un post par son id
+        async getPostById(req, res) {
+        try {
+            const post = await Post.findByPk(id);
+            if (!post) {
+                return res.status(404).json({ message: 'Post introuvable' });
+            }
+        const { id } = req.params;}
+        catch (error) {
+            console.error('Erreur lors de la récupération du post :', error);
+            res.status(500).json({ message: 'Erreur serveur' });
+        }
+    }
     // ajout de la methode de suppression d'un post 
-    async deletePost(req,res){
+         async deletePost(req,res){
 
         // on recupere l'id du post a supprimer
         const {id} = req.params;
